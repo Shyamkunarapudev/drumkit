aed2fb6e762edb8d3e330ff1237db659b5d62e86
Shyam Kumar <153950766+Shyamkunarapudev@users.noreply.github.com>
2024-09-19 16:59:08 +0530

    Add files via upload
    I made drum kit project

---
+++ index.js
@@ -0,0 +1,48 @@
+let drumsLength = document.querySelectorAll(".drum").length;
+
+for(let i = 0; i < drumsLength; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", drumKit);
+
+    function drumKit() {
+
+        let pressing = this.innerHTML;
+
+        switch (pressing) {
+            case "w":
+                let tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                let tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                let tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                let tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                let kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+
+                break;
+            case "k":
+                let snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            case "l":
+                let crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+
+        
+            default: console.log("No Warries");
+                break;
+        }
+
+    }
+
+}
